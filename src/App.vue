be207ee2214f2789d7887dc6e0374b8ac948f933
have1568 <1006369654@qq.com>
2021-11-23 01:04:16 +0800

    初始换项目，新建基本框架，页面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,93 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <!-- App.vue -->
+
+  <v-app v-if="$route.meta.public != true">
+    <v-navigation-drawer
+        permanent
+        expand-on-hover
+        app
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-card class="mx-auto overflow-hidden">
+      <v-app-bar color="blue" dark app>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+      </v-app-bar>
+    </v-card>
+
+    <!-- 根据应用组件来调整你的内容 -->
+    <v-main>
+      <!-- 给应用提供合适的间距 -->
+      <v-container fluid>
+        <!-- 如果使用 vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer padless>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} —
+        <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
+  <v-app v-else>
+    <router-view></router-view>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,15 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '*',
+    meta: {
+      public: true,
+    },
+    redirect: {
+      path: '/404'
+    }
+  }, 
+  {
     path: '/',
     name: 'Home',
     component: Home
@@ -17,7 +26,65 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/404',
+    meta: {
+      public: true,
+    },
+    name: 'NotFound',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `@/views/NotFound.vue`
+    )
+  },
+  {
+    path: '/403',
+    meta: {
+      public: true,
+    },
+    name: 'AccessDenied',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `@/views/Deny.vue`
+    )
+  },
+  {
+    path: '/500',
+    meta: {
+      public: true,
+    },
+    name: 'ServerError',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `@/views/Error.vue`
+    )
+  },
+  {
+    path: '/login',
+    meta: {
+      public: true,
+    },
+    name: 'Login',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `@/views/Login.vue`
+    )
+  },
+  {
+    path: '/user_manage',
+    name: 'UserManage',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `@/components/UserManage.vue`
+    )
+  },
+
 ]
 
 const router = new VueRouter({
